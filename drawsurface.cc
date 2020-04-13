Micah314Smith/asymptote-open

commit 87a802cfeb9b86240fc33e8264320e772182d87e

Remove Triple type from Bezier patches.

drawsurface.cc
@@ -85,126 +85,124 @@ void drawSurface::bounds(const double* t, bbox3& b)
   double X,Y,Z;
   
   if(straight) {
-    Triple *Vertices;
+    triple *Vertices;
     if(t == NULL) Vertices=vertices;
     else {
-      static Triple buf[4];
+      triple buf[4];
       Vertices=buf;
-      transformTriples(t,4,Vertices,vertices);
+      for(int i=0; i < 4; ++i)
+        Vertices[i]=t*vertices[i];
     }
   
-    double *v=Vertices[0];
-    x=v[0];
-    y=v[1];
-    z=v[2];
+    triple v=vertices[0];
+    x=v.getx();
+    y=v.gety();
+    z=v.getz();
     X=x;
     Y=y;
     Z=z;
     for(size_t i=1; i < 4; ++i) {
-      double *v=Vertices[i];
-      double vx=v[0];
+      triple v=vertices[i];
+      double vx=v.getx();
       x=min(x,vx);
       X=max(X,vx);
-      double vy=v[1];
+      double vy=v.gety();
       y=min(y,vy);
       Y=max(Y,vy);
-      double vz=v[2];
+      double vz=v.getz();
       z=min(z,vz);
       Z=max(Z,vz);
     }
   } else {
-    Triple* Controls;
-    if(t == NULL) Controls=controls;
-    else {
-      static Triple buf[16];
-      Controls=buf;
-      transformTriples(t,16,Controls,controls);
-    }
-
-    static double c1[16];
-
-    for(int i=0; i < 16; ++i)
-      c1[i]=Controls[i][0];
-    double c0=c1[0];
-    double fuzz=sqrtFuzz*run::norm(c1,16);
-    x=bound(c1,min,b.empty ? c0 : min(c0,b.left),fuzz,maxdepth);
-    X=bound(c1,max,b.empty ? c0 : max(c0,b.right),fuzz,maxdepth);
-    
-    for(int i=0; i < 16; ++i)
-      c1[i]=Controls[i][1];
-    c0=c1[0];
-    fuzz=sqrtFuzz*run::norm(c1,16);
-    y=bound(c1,min,b.empty ? c0 : min(c0,b.bottom),fuzz,maxdepth);
-    Y=bound(c1,max,b.empty ? c0 : max(c0,b.top),fuzz,maxdepth);
-    
-    for(int i=0; i < 16; ++i)
-      c1[i]=Controls[i][2];
-    c0=c1[0];
-    fuzz=sqrtFuzz*run::norm(c1,16);
-    z=bound(c1,min,b.empty ? c0 : min(c0,b.lower),fuzz,maxdepth);
-    Z=bound(c1,max,b.empty ? c0 : max(c0,b.upper),fuzz,maxdepth);
-  }
-    
-  b.add(x,y,z);
-  b.add(X,Y,Z);
+    static double cx[16];
+    static double cy[16];
+    static double cz[16];
   
-  if(t == NULL) {
-    Min=triple(x,y,z);
-    Max=triple(X,Y,Z);
+    if(t == NULL) {
+      for(int i=0; i < 16; ++i) {
+        triple v=controls[i];
+        cx[i]=v.getx();
+        cy[i]=v.gety();
+        cz[i]=v.getz();
+      }
+    } else {
+      for(int i=0; i < 16; ++i) {
+        triple v=t*controls[i];
+        cx[i]=v.getx();
+        cy[i]=v.gety();
+        cz[i]=v.getz();
+      }
+    }
+    
+    double c0=cx[0];
+    double fuzz=sqrtFuzz*run::norm(cx,16);
+    x=bound(cx,min,b.empty ? c0 : min(c0,b.left),fuzz,maxdepth);
+    X=bound(cx,max,b.empty ? c0 : max(c0,b.right),fuzz,maxdepth);
+    
+    c0=cy[0];
+    fuzz=sqrtFuzz*run::norm(cy,16);
+    y=bound(cy,min,b.empty ? c0 : min(c0,b.bottom),fuzz,maxdepth);
+    Y=boundtri(cy,max,b.empty ? c0 : max(c0,b.top),fuzz,maxdepth);
+    
+    c0=cz[0];
+    fuzz=sqrtFuzz*run::norm(cz,16);
+    z=bound(cz,min,b.empty ? c0 : min(c0,b.lower),fuzz,maxdepth);
+    Z=bound(cz,max,b.empty ? c0 : max(c0,b.upper),fuzz,maxdepth);
+    
+    b.add(x,y,z);
+    b.add(X,Y,Z);
+  
+    if(t == NULL) {
+      Min=triple(x,y,z);
+      Max=triple(X,Y,Z);
+    }
   }
 }
 
 void drawSurface::ratio(const double* t, pair &b, double (*m)(double, double),
                         double fuzz, bool &first)
 {
+  static triple buf[16];
   if(straight) {
-    Triple *Vertices;
+    triple *Vertices;
     if(t == NULL) Vertices=vertices;
     else {
-      static Triple buf[4];
       Vertices=buf;
-      transformTriples(t,4,Vertices,vertices);
+      for(int i=0; i < 4; ++i)
+        Vertices[i]=t*vertices[i];
     }
   
     if(first) {
       first=false;
-      double *ci=Vertices[0];
-      triple v=triple(ci[0],ci[1],ci[2]);
+      triple v=Vertices[0];
       b=pair(xratio(v),yratio(v));
     }
   
     double x=b.getx();
     double y=b.gety();
     for(size_t i=0; i < 4; ++i) {
-      double *ci=Vertices[i];
-      triple v=triple(ci[0],ci[1],ci[2]);
+      triple v=Vertices[i];
       x=m(x,xratio(v));
       y=m(y,yratio(v));
     }
     b=pair(x,y);
   } else {
-    Triple* Controls;
+    triple* Controls;
     if(t == NULL) Controls=controls;
     else {
-      static Triple buf[16];
       Controls=buf;
-      transformTriples(t,16,Controls,controls);
+      for(int i=0; i < 16; ++i)
+        Controls[i]=t*controls[i];
     }
 
-    static triple c3[16];
-    for(int i=0; i < 16; ++i) {
-      double *ci=Controls[i];
-      c3[i]=triple(ci[0],ci[1],ci[2]);
-    }
-  
     if(first) {
-      triple v=c3[0];
+      triple v=Controls[0];
       b=pair(xratio(v),yratio(v));
       first=false;
     }
   
-    b=pair(bound(c3,m,xratio,b.getx(),fuzz,maxdepth),
-           bound(c3,m,yratio,b.gety(),fuzz,maxdepth));
+    b=pair(bound(Controls,m,xratio,b.getx(),fuzz,maxdepth),
+           bound(Controls,m,yratio,b.gety(),fuzz,maxdepth));
   }
 }
 
@@ -228,9 +226,9 @@ bool drawSurface::write(prcfile *out, unsigned int *, double, groupsmap&)
 
 // return the perpendicular displacement of a point z from the plane
 // through u with unit normal n.
-inline triple displacement2(const Triple& z, const Triple& u, const triple& n)
+inline triple displacement2(const triple& z, const triple& u, const triple& n)
 {
-  triple Z=triple(z)-triple(u);
+  triple Z=z-u;
   return n != triple(0,0,0) ? dot(Z,n)*n : Z;
 }
 
@@ -241,13 +239,10 @@ inline triple maxabs(triple u, triple v)
                 max(fabs(u.getz()),fabs(v.getz())));
 }
 
-inline triple displacement1(const Triple& z0, const Triple& c0,
-                           const Triple& c1, const Triple& z1)
+inline triple displacement1(const triple& z0, const triple& c0,
+                            const triple& c1, const triple& z1)
 {
-  triple Z0(z0);
-  triple Z1(z1);
-  return maxabs(displacement(triple(c0[0],c0[1],c0[2]),Z0,Z1),
-                displacement(triple(c1[0],c1[1],c1[2]),Z0,Z1));
+  return maxabs(displacement(c0,z0,z1),displacement(c1,z0,z1));
 }
 
 void drawSurface::displacement()

drawsurface.h
@@ -20,8 +20,8 @@ void storecolor(GLfloat *colors, int i, const vm::array &pens, int j);
 
 class drawSurface : public drawElement {
 protected:
-  Triple *controls;
-  Triple vertices[4];
+  triple *controls;
+  triple vertices[4];
   triple center;
   bool straight;
   RGBAColour diffuse;
@@ -63,20 +63,20 @@ public:
     vm::array *g3=vm::read<vm::array*>(g,3);
     if(checkArray(g0) != 4 || checkArray(g3) != 4)
       reportError(wrongsize);
-    store(vertices[0],vm::read<triple>(g0,0));
-    store(vertices[1],vm::read<triple>(g0,3));
-    store(vertices[2],vm::read<triple>(g3,0));
-    store(vertices[3],vm::read<triple>(g3,3));
+    vertices[0]=vm::read<triple>(g0,0);
+    vertices[1]=vm::read<triple>(g0,3);
+    vertices[2]=vm::read<triple>(g3,0);
+    vertices[3]=vm::read<triple>(g3,3);
     
     if(!havenormal || !straight) {
       size_t k=0;
-      controls=new(UseGC) Triple[16];
+      controls=new(UseGC) triple[16];
       for(size_t i=0; i < 4; ++i) {
         vm::array *gi=vm::read<vm::array*>(g,i);
         if(checkArray(gi) != 4) 
           reportError(wrongsize);
         for(size_t j=0; j < 4; ++j)
-          store(controls[k++],vm::read<triple>(gi,j));
+          controls[k++]=vm::read<triple>(gi,j);
       }
     } else controls=NULL;
     
@@ -106,13 +106,15 @@ public:
     invisible(s->invisible),
     interaction(s->interaction), prc(s->prc) { 
     
-    transformTriples(t,4,vertices,s->vertices);
+    for(int i=0; i < 4; ++i)
+      vertices[i]=t*s->vertices[i];
     
     if(s->controls) {
-      controls=new(UseGC) Triple[16];
-      transformTriples(t,16,controls,s->controls);
+      controls=new(UseGC) triple[16];
+      for(int i=0; i < 16; ++i)
+        controls[i]=t*s->controls[i];
     } else controls=NULL;
-    
+  
 #ifdef HAVE_GL
     center=t*s->center;
     normal=transformNormal(t,s->normal);

prc/oPRCFile.cc
@@ -1455,25 +1455,6 @@ PRCgroup& oPRCFile::findGroup()
   wire.curve = curve;                                     \
   wire.style = addColour(c);
 
-#define ADDFACE(surftype)                                 \
-  PRCgroup &group = findGroup();                          \
-  group.faces.push_back(PRCface());                       \
-  PRCface& face = group.faces.back();                     \
-  surftype *surface = new surftype;                       \
-  face.face = new PRCFace;                                \
-  face.face->base_surface = surface;                      \
-  face.transparent = m.alpha < 1.0;                       \
-  face.style = addMaterial(m);
-
-#define ADDCOMPFACE                                       \
-  PRCgroup &group = findGroup();                          \
-  group.compfaces.push_back(PRCcompface());               \
-  PRCcompface& face = group.compfaces.back();             \
-  PRCCompressedFace *compface = new PRCCompressedFace;    \
-  face.face = compface;                                   \
-  face.transparent = m.alpha < 1.0;                       \
-  face.style = addMaterial(m);
-
 void oPRCFile::addPoint(const double P[3], const RGBAColour &c, double w)
 {
   PRCgroup &group = findGroup();
@@ -1695,20 +1676,6 @@ uint32_t oPRCFile::createQuadMesh(uint32_t nP, const double P[][3], uint32_t nI,
   return tess_index;
 }
 
-void oPRCFile::addQuad(const double P[][3], const RGBAColour C[])
-{
-  PRCgroup &group = findGroup();
-
-  group.quads.push_back(PRCtessquad());
-  PRCtessquad &quad = group.quads.back();
-  for(size_t i = 0; i < 4; i++)
-  {
-    quad.vertices[i].x = P[i][0];
-    quad.vertices[i].y = P[i][1];
-    quad.vertices[i].z = P[i][2];
-    quad.colours[i] = C[i];
-  }
-}
 /*
 void oPRCFile::addTriangle(const double P[][3], const double T[][2], uint32_t style_index)
 {
@@ -1841,100 +1808,6 @@ void oPRCFile::addCurve(uint32_t d, uint32_t n, const double cP[][3], const doub
   curve->knot.resize(d+n+1);
   for(uint32_t i = 0; i < d+n+1; i++)
     curve->knot[i] = k[i];
-}
-
-void oPRCFile::addRectangle(const double P[][3], const PRCmaterial &m)
-{
-  PRCgroup &group = findGroup();
-  if(group.options.tess)
-  {
-    group.rectangles.push_back(PRCtessrectangle());
-    PRCtessrectangle &rectangle = group.rectangles.back();
-    rectangle.style = addMaterial(m);
-    for(size_t i = 0; i < 4; i++)
-    {
-       rectangle.vertices[i].x = P[i][0];
-       rectangle.vertices[i].y = P[i][1];
-       rectangle.vertices[i].z = P[i][2];
-    }
-  }
-  else if(group.options.compression == 0.0)
-  {
-    ADDFACE(PRCNURBSSurface)
-
-    surface->is_rational = false;
-    surface->degree_in_u = 1;
-    surface->degree_in_v = 1;
-    surface->control_point.resize(4);
-    for(size_t i = 0; i < 4; ++i)
-    {
-        surface->control_point[i].x = P[i][0];
-        surface->control_point[i].y = P[i][1];
-        surface->control_point[i].z = P[i][2];
-    }
-    surface->knot_u.resize(4);
-    surface->knot_v.resize(4);
-    surface->knot_v[0] = surface->knot_u[0] = 1;
-    surface->knot_v[1] = surface->knot_u[1] = 3;
-    surface->knot_v[2] = surface->knot_u[2] = 4;
-    surface->knot_v[3] = surface->knot_u[3] = 4;
-  }
-  else
-  {
-    ADDCOMPFACE
-
-    compface->degree = 1;
-    compface->control_point.resize(4);
-    for(size_t i = 0; i < 4; ++i)
-    {
-        compface->control_point[i].x = P[i][0];
-        compface->control_point[i].y = P[i][1];
-        compface->control_point[i].z = P[i][2];
-    }
-  }
-}
-
-void oPRCFile::addPatch(const double cP[][3], const PRCmaterial &m)
-{
-  PRCgroup &group = findGroup();
-  if(group.options.compression == 0.0)
-  {
-    ADDFACE(PRCNURBSSurface)
-
-    surface->is_rational = false;
-    surface->degree_in_u = 3;
-    surface->degree_in_v = 3;
-    surface->control_point.resize(16);
-    for(size_t i = 0; i < 16; ++i)
-    {
-        surface->control_point[i].x = cP[i][0];
-        surface->control_point[i].y = cP[i][1];
-        surface->control_point[i].z = cP[i][2];
-    }
-    surface->knot_u.resize(8);
-    surface->knot_v.resize(8);
-    surface->knot_v[0] = surface->knot_u[0] = 1;
-    surface->knot_v[1] = surface->knot_u[1] = 1;
-    surface->knot_v[2] = surface->knot_u[2] = 1;
-    surface->knot_v[3] = surface->knot_u[3] = 1;
-    surface->knot_v[4] = surface->knot_u[4] = 2;
-    surface->knot_v[5] = surface->knot_u[5] = 2;
-    surface->knot_v[6] = surface->knot_u[6] = 2;
-    surface->knot_v[7] = surface->knot_u[7] = 2;
-  }
-  else
-  {
-    ADDCOMPFACE
-
-    compface->degree = 3;
-    compface->control_point.resize(16);
-    for(size_t i = 0; i < 16; ++i)
-    {
-        compface->control_point[i].x = cP[i][0];
-        compface->control_point[i].y = cP[i][1];
-        compface->control_point[i].z = cP[i][2];
-    }
-  }
 }
 
 void oPRCFile::addSurface(uint32_t dU, uint32_t dV, uint32_t nU, uint32_t nV,

prc/oPRCFile.h
@@ -951,10 +951,137 @@ uint32_t nN, const V N[],  const uint32_t NI[][3],
     void addLine(uint32_t n, const double P[][3], const RGBAColour &c, double w=1.0);
     void addBezierCurve(uint32_t n, const double cP[][3], const RGBAColour &c);
     void addCurve(uint32_t d, uint32_t n, const double cP[][3], const double *k, const RGBAColour &c, const double w[]);
-    void addQuad(const double P[][3], const RGBAColour C[]);
+template<class V>
+void addQuad(const V P[], const RGBAColour C[])
+{
+  PRCgroup &group = findGroup();
 
-    void addRectangle(const double P[][3], const PRCmaterial &m);
-    void addPatch(const double cP[][3], const PRCmaterial &m);
+  group.quads.push_back(PRCtessquad());
+  PRCtessquad &quad = group.quads.back();
+  for(size_t i = 0; i < 4; i++)
+  {
+    quad.vertices[i].x = X(P[i]);
+    quad.vertices[i].y = Y(P[i]);
+    quad.vertices[i].z = Z(P[i]);
+    quad.colours[i] = C[i];
+  }
+}
+
+#define ADDFACE(surftype)                                 \
+  PRCgroup &group = findGroup();                          \
+  group.faces.push_back(PRCface());                       \
+  PRCface& face = group.faces.back();                     \
+  surftype *surface = new surftype;                       \
+  face.face = new PRCFace;                                \
+  face.face->base_surface = surface;                      \
+  face.transparent = m.alpha < 1.0;                       \
+  face.style = addMaterial(m);
+
+#define ADDCOMPFACE                                       \
+  PRCgroup &group = findGroup();                          \
+  group.compfaces.push_back(PRCcompface());               \
+  PRCcompface& face = group.compfaces.back();             \
+  PRCCompressedFace *compface = new PRCCompressedFace;    \
+  face.face = compface;                                   \
+  face.transparent = m.alpha < 1.0;                       \
+  face.style = addMaterial(m);
+
+template<class V>
+void addRectangle(const V P[], const PRCmaterial &m)
+{
+  PRCgroup &group = findGroup();
+  if(group.options.tess)
+  {
+    group.rectangles.push_back(PRCtessrectangle());
+    PRCtessrectangle &rectangle = group.rectangles.back();
+    rectangle.style = addMaterial(m);
+    for(size_t i = 0; i < 4; i++)
+    {
+      rectangle.vertices[i].x = X(P[i]);
+      rectangle.vertices[i].y = Y(P[i]);
+      rectangle.vertices[i].z = Z(P[i]);
+    }
+  }
+  else if(group.options.compression == 0.0)
+  {
+    ADDFACE(PRCNURBSSurface)
+
+    surface->is_rational = false;
+    surface->degree_in_u = 1;
+    surface->degree_in_v = 1;
+    surface->control_point.resize(4);
+    for(size_t i = 0; i < 4; ++i)
+    {
+      surface->control_point[i].x = X(P[i]);
+      surface->control_point[i].y = Y(P[i]);
+      surface->control_point[i].z = Z(P[i]);
+    }
+    surface->knot_u.resize(4);
+    surface->knot_v.resize(4);
+    surface->knot_v[0] = surface->knot_u[0] = 1;
+    surface->knot_v[1] = surface->knot_u[1] = 3;
+    surface->knot_v[2] = surface->knot_u[2] = 4;
+    surface->knot_v[3] = surface->knot_u[3] = 4;
+  }
+  else
+  {
+    ADDCOMPFACE
+
+    compface->degree = 1;
+    compface->control_point.resize(4);
+    for(size_t i = 0; i < 4; ++i)
+    {
+      compface->control_point[i].x = X(P[i]);
+      compface->control_point[i].y = Y(P[i]);
+      compface->control_point[i].z = Z(P[i]);
+    }
+  }
+}
+
+template<class V>
+void addPatch(const V cP[], const PRCmaterial &m)
+{
+  PRCgroup &group = findGroup();
+  if(group.options.compression == 0.0)
+  {
+    ADDFACE(PRCNURBSSurface)
+
+    surface->is_rational = false;
+    surface->degree_in_u = 3;
+    surface->degree_in_v = 3;
+    surface->control_point.resize(16);
+    for(size_t i = 0; i < 16; ++i)
+    {
+      surface->control_point[i].x = X(cP[i]);
+      surface->control_point[i].y = Y(cP[i]);
+      surface->control_point[i].z = Z(cP[i]);
+    }
+    surface->knot_u.resize(8);
+    surface->knot_v.resize(8);
+    surface->knot_v[0] = surface->knot_u[0] = 1;
+    surface->knot_v[1] = surface->knot_u[1] = 1;
+    surface->knot_v[2] = surface->knot_u[2] = 1;
+    surface->knot_v[3] = surface->knot_u[3] = 1;
+    surface->knot_v[4] = surface->knot_u[4] = 2;
+    surface->knot_v[5] = surface->knot_u[5] = 2;
+    surface->knot_v[6] = surface->knot_u[6] = 2;
+    surface->knot_v[7] = surface->knot_u[7] = 2;
+  }
+  else
+  {
+    ADDCOMPFACE
+
+    compface->degree = 3;
+    compface->control_point.resize(16);
+    for(size_t i = 0; i < 16; ++i)
+    {
+      compface->control_point[i].x = X(cP[i]);
+      compface->control_point[i].y = Y(cP[i]);
+      compface->control_point[i].z = Z(cP[i]);
+    }
+  }
+}
+
     void addSurface(uint32_t dU, uint32_t dV, uint32_t nU, uint32_t nV,
      const double cP[][3], const double *kU, const double *kV, const PRCmaterial &m,
      const double w[]);
@@ -968,7 +1095,7 @@ uint32_t nN, const V N[],  const uint32_t NI[][3],
 #undef PRCTRANSFORM
 #undef PRCCARTRANSFORM
 #undef PRCGENTRANSFORM
-
+#undef ADDCOMPFACE
 
     uint32_t addPicture(EPRCPictureDataFormat format, uint32_t size, const uint8_t *picture, uint32_t width=0, uint32_t height=0,
       std::string name="", uint32_t fileStructure=0)

commit 1589f75b0a863374a63e9be994754b9e233c6249

Fix Bezier patch bounds bug introduced in 87a802cfeb9b86240fc33e8264320e772182d87e.

doc/asymptote.texi
@@ -2216,7 +2216,7 @@ current locale;
 
 @item string time(string format="%a %b %d %T %Z %Y")
 @cindex @code{time}
-@cindex @code{date}
+@cindex date
 @cindex @code{strftime}
 returns the current time formatted by the ANSI C routine
 @code{strftime} according to the string @code{format} using the current

drawsurface.cc
@@ -104,14 +104,14 @@ void drawBezierPatch::bounds(const double* t, bbox3& b)
     double cz[16];
 
     if(t == NULL) {
-      for(int i=0; i < 16; ++i) {
+      for(unsigned int i=0; i < 16; ++i) {
         triple v=controls[i];
         cx[i]=v.getx();
         cy[i]=v.gety();
         cz[i]=v.getz();
       }
     } else {
-      for(int i=0; i < 16; ++i) {
+      for(unsigned int i=0; i < 16; ++i) {
         triple v=t*controls[i];
         cx[i]=v.getx();
         cy[i]=v.gety();
@@ -127,7 +127,7 @@ void drawBezierPatch::bounds(const double* t, bbox3& b)
     c0=cy[0];
     fuzz=Fuzz*run::norm(cy,16);
     y=bound(cy,min,b.empty ? c0 : min(c0,b.bottom),fuzz,maxdepth);
-    Y=boundtri(cy,max,b.empty ? c0 : max(c0,b.top),fuzz,maxdepth);
+    Y=bound(cy,max,b.empty ? c0 : max(c0,b.top),fuzz,maxdepth);
 
     c0=cz[0];
     fuzz=Fuzz*run::norm(cz,16);
@@ -359,7 +359,7 @@ void drawBezierTriangle::bounds(const double* t, bbox3& b)
         cz[i]=v.getz();
       }
     }
-    
+
     double c0=cx[0];
     double fuzz=Fuzz*run::norm(cx,10);
     x=boundtri(cx,min,b.empty ? c0 : min(c0,b.left),fuzz,maxdepth);